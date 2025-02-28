6cc96791dce4860054e2db0e33fb48e746e321a0
izathomaz <izabele.tthomaz@gmail.com>
2025-02-28 13:46:35 -0300

    Add files via upload

---
+++ Gerador_estruturaprojetos.bat
@@ -0,0 +1,79 @@
+echo off
+
+mkdir "EMPRESA STARXX"
+cd "EMPRESA STARXX"
+mkdir ITAPETININGA
+mkdir TATUI
+cd ITAPETININGA
+mkdir SETORES
+cd SETORES
+mkdir COMPRAS
+mkdir CONTABILIDADE
+mkdir FINANCEIRO
+mkdir FISCAL
+mkdir RH
+mkdir TI
+mkdir VENDAS
+cd ..
+cd ..
+cd TATUI
+mkdir SETORES
+cd SETORES
+mkdir COMPRAS
+mkdir CONTABILIDADE
+mkdir FINANCEIRO
+mkdir FISCAL
+mkdir RH
+mkdir TI
+mkdir VENDAS
+cd COMPRAS
+type nul > info.txt
+cd ..
+cd CONTABILIDADE
+type nul > info.txt
+cd ..
+cd FINANCEIRO
+type nul > info.txt
+cd ..
+cd FISCAL
+type nul > info.txt
+cd ..
+cd RH
+type nul > info.txt
+cd ..
+cd TI
+type nul > info.txt
+cd ..
+cd VENDAS
+type nul > info.txt
+cd ..
+cd ..
+cd ..
+cd ITAPETININGA
+cd SETORES
+cd COMPRAS
+type nul > info.txt
+cd ..
+cd CONTABILIDADE
+type nul > info.txt
+cd ..
+cd FINANCEIRO
+type nul > info.txt
+cd ..
+cd FISCAL
+type nul > info.txt
+cd ..
+cd RH
+type nul > info.txt
+cd ..
+cd TI
+type nul > info.txt
+cd ..
+cd VENDAS
+type nul > info.txt
+cd ..
+cd ..
+cd ..
+tree
+
+pause
